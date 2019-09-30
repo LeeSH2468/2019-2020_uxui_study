7d2c94019e93b20fc6cbbdc0a81da55d0e9a2306
Leesohi <lsohi2468@gmail.com>
2019-09-30 18:14:05 +0900

    실습

---
+++ practice/d_js_basic/js/base/basic_02.js
@@ -0,0 +1,18 @@
+//basic_02.js
+console.log("base_02.js 연결완료!");
+
+console.log(10+10);
+console.log("10" +100);
+console.log("10"=== 10);
+
+var 낄끼빠빠 ="낄떄끼등가";
+console.log(낄끼빠빠);
+
+var coffee="인생의 쓴맛,으른의맛, 포션";
+console.log(coffee);
+
+coffee ="사치, 오늘 하루를 무사히.....";
+console.log(coffee);
+
+//;작성하는곳 : ";"의 의미는 기능종료를 뜻함.
+
