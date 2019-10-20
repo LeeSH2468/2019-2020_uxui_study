335f3012990dc190a27f2142eb77de9a328f0899
Leesohi <lsohi2469@gmail.com>
2019-10-20 19:45:05 +0900

    내셔널- 페이지3(뉴스메인)

--- document/National_Geofraphic/code/js/src/national_01_main.js
+++ document/National_Geofraphic/code/js/src/national_01_main.js
@@ -1,32 +1,6 @@
 // national_01_main.js
 (function($){
-  console.log($.fn);
 
-  const magaBtn = $('.maga');
-  const mPopup = $('.m_popup');
-  const closePopBtn = $('.m_close_btn');
-
-  magaBtn.on('click',function(e){
-    console.log(e);
-    e.preventDefault();
-    mPopup.fadeIn();
-  });
-
-  closePopBtn.on('click',function(e){
-    console.log(e);
-    e.preventDefault();
-    mPopup.fadeOut();    
-  });
-
-  // background mousemove ==========================
-  const viewBg =$('.view_pic_distant');
-
-  viewBg.on('mousemove', function(e){
-    console.log(e);
-    let x = event.offsetX;
-    let y = event.offsetY;
-   viewBg.css({"transform":`translate(-${x}%,-${y}%)`});
-  });
   
 
 })(jQuery);
--- document/National_Geofraphic/code/js/src/national_header_footer.js
+++ document/National_Geofraphic/code/js/src/national_header_footer.js
@@ -1,5 +1,6 @@
 //national_header_footer.js
 (function($){
+  // load
   const wrap = $('#wrap');
   wrap.prepend('<div class="head_wrap"></div>');
   const headWrap = $('.head_wrap');
@@ -10,6 +11,36 @@
   headWrap.load('./temp/header.html',function(){
   });
   footWrap.load('./temp/footer.html',function(){
+  });
+
+
+  // magazines
+  console.log($.fn);
+
+  const magaBtn = $('.maga');
+  const mPopup = $('.m_popup');
+  const closePopBtn = $('.m_close_btn');
+
+  magaBtn.on('click',function(e){
+    console.log(e);
+    e.preventDefault();
+    mPopup.fadeIn();
+  });
+
+  closePopBtn.on('click',function(e){
+    console.log(e);
+    e.preventDefault();
+    mPopup.fadeOut();    
+  });
+
+  // background mousemove ==========================
+  const viewBg =$('.view_pic_distant');
+
+  viewBg.on('mousemove', function(e){
+    console.log(e);
+    let x = event.offsetX;
+    let y = event.offsetY;
+   viewBg.css({"transform":`translate(-${x}%,-${y}%)`});
   });
 
 })(jQuery);
