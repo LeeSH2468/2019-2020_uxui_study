5251462581212f5b490afbaf95c37d89aff5e273
LeeSH <lsohi2468@gmail.com>
2019-10-25 18:08:45 +0900

    slide만들기 scss

--- practice/f_new_example/public/js/src/main.js
+++ practice/f_new_example/public/js/src/main.js
@@ -3,17 +3,21 @@
   const body = $('body');
   const wrap = $('#wrap');//#wrap
 
-  wrap.append('<section id="viewBox"></section>'); //#viewBox 생성
-  const viewBox = $('#viewBox');
+
   //script 내용 간편작성 함수 생성
   const jsLink = (temp) => {
-    body.append(`<script src="../js/src/temp/${temp}"></script>`);
+    return body.append(`<script src="../js/src/temp/${temp}"></script>`);
   };
+  // 광고영역 생성 =======================
+  wrap.append('<section id="viewBox"></section>'); //#viewBox 생성
+  const viewBox = $('#viewBox');
+
 
 
   
   viewBox.load('./temp/slide_01.html',function(){
     jsLink('slide_01.js');
   });
+//================================================
 
 })(jQuery);
