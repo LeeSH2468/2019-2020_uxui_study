d49701a4eaf69427cd542d13ca82e3dadebaed6e
leesohi <lsohi2468@gmail.com>
2019-12-12 18:09:20 +0900

    my-nome

--- document/identity/my_home/js/src/lee_home.js
+++ document/identity/my_home/js/src/lee_home.js
@@ -4,17 +4,22 @@
   const li_01 = gnb.find('li').eq(0).children('a');
   const li_02 = gnb.find('li').eq(1).children('a');
   const li_03 = gnb.find('li').eq(2).children('a');
+  const li_04 = gnb.find('li').eq(3).children('a');
+  //각도 배열
   
-  li_01.on('click',function(e){
-    logo.css({'transform':'rotate(40deg)'});
+  
+  li_01.on('click mouseenter',function(e){
+    logo.css({'transform':'rotate(50deg)'});
   });
-  li_02.on('click',function(e){
-    logo.css({'transform':'rotate(-10deg)'});
+  li_02.on('click mouseenter',function(e){
+    logo.css({'transform':'rotate(10deg)'});
   });
-  li_03.on('click',function(e){
-    logo.css({'transform':'rotate(-100deg)'});
+  li_03.on('click mouseenter',function(e){
+    logo.css({'transform':'rotate(-80deg)'});
   });
-
+  li_04.on('click mouseenter',function(e){
+    logo.css({'transform':'rotate(-108deg)'});
+  });
 
 
 
