05bf32d9adf508964bfb797772172137a0404535
leesohi <lsohi2468@gmail.com>
2019-12-18 18:32:48 +0900

    copy page

--- document/identity/my_home/js/src/foot.js
+++ document/identity/my_home/js/src/foot.js
@@ -1,37 +1,37 @@
-// const c = document.querySelector('canvas');
-// const ctx = c.getContext('2d');
-// const foot = document.querySelector('#footBox');
-// document.body.appendChild(c);
-// c.width = 1920;
-// c.height = 200;
+const c = document.querySelector('.canvas');
+const ctx = c.getContext('2d');
+const foot = document.querySelector('#footBox');
 
-// let perm = [];
-// while (perm.length < 255){
-//   while(perm.includes(val = Math.floor(Math.random()*255)));
-//   perm.push(val);
-// }
+c.width = 1903;
+c.height = 200;
 
-// let lerp = (a,b,t) => a + (b-a) * (1-Math.cos(t*Math.PI))/2;
-// let noise = x =>{
-//   x = x * 0.01 % 255;
-//   return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
-// }
+let perm = [];
+while (perm.length < 255){
+  while(perm.includes(val = Math.floor(Math.random()*255)));
+  perm.push(val);
+}
 
-// let t = 0;
-// function loop(){
-//   t++;
-//   ctx.fillStyle = "#19f";
-//   ctx.fillRect(0,0,c.width ,c.height);
+let lerp = (a,b,t) => a + (b-a) * (1-Math.cos(t*Math.PI))/2;
+let noise = x =>{
+  x = x * 0.01 % 255;
+  return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
 
-//   ctx.fillStyle = "black";
-//   ctx.beginPath();
-//   ctx.moveTo(0,c.height);
-//   for(let i = 0; i <c.width;i++)
-//     ctx.lineTo(i,c.height - noise(t + i)* 0.25);
+let t = 0;
+function loop(){
+  t++;
+  ctx.fillStyle = "#ddd";
+  ctx.fillRect(0,0,c.width ,c.height);
 
-//     ctx.lineTo(c.width, c.height);
-//     ctx.fill();
-//     requestAnimationFrame(loop);
-// }
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  ctx.moveTo(0,c.height);
+  for(let i = 0; i <c.width;i++)
+    ctx.lineTo(i,c.height - noise(t + i)* 0.25);
 
-// loop();
+    ctx.lineTo(c.width, c.height);
+    ctx.fill();
+    requestAnimationFrame(loop);
+}
+
+loop();
